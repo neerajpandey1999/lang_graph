7e52cd9b18821077f7b79f537bdd1318bdf31b5a
neerajpandey1999 <neerajpandey0531@gmail.com>
2024-09-21 12:28:42 +0530

    refactor code

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ os.environ["LANGCHAIN_PROJECT"]="CourseLanggraph"
 from langchain_groq import ChatGroq
 
 llm=ChatGroq(groq_api_key=groq_api_key,model_name="Gemma2-9b-It")
-print(llm)
 
 from typing import Annotated
 from typing_extensions import TypedDict
@@ -25,9 +24,6 @@ from langgraph.graph.message import add_messages
 
 
 class State(TypedDict):
-  # Messages have the type "list". The `add_messages` function
-    # in the annotation defines how this state key should be updated
-    # (in this case, it appends messages to the list, rather than overwriting them)
   messages:Annotated[list,add_messages]
 
 graph_builder=StateGraph(State)
